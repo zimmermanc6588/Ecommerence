8d697c0e6ffee57eb2230ec7d410f4f02f703fc4
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-10-02 14:57:52 -0500

    updated information in models

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,7 +11,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type:DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+ 
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type:DataTypes.DECIMAL,
+       allowNull: false, 
+       validate: {
+        isDecimal:true,
+        }
+    
   },
+
+    stock: {
+  type:DataTypes.INTEGER, 
+  allowNull: false,
+  defaultValue: 10,
+  validate: {
+    isNumeric:true,
+    }
+  }
+},
+
+category_id: {
+  type:DataTypes.INTEGER, 
+  
+},
+
   {
     sequelize,
     timestamps: false,
